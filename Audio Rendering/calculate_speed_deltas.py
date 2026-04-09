289c125180517d09e287a79ee980963e7a3a6975
TunZ04 <jordantun14@gmail.com>
2026-04-09 23:24:39 +0100

    fixed desync between force alignment and word speeds

--- Audio Rendering/calculate_speed_deltas.py
+++ Audio Rendering/calculate_speed_deltas.py
@@ -26,31 +26,54 @@ audio_path = f"Input/{NAME}.wav"
 
 # measuring sentence similarity and wordcounts per sentence #
 
-model_directory = "Models/sentence_similarity"
+model_directory = "Model_Data/sentence_similarity"
 
-if os.path.exists(model_directory):
-  model = SentenceTransformer(model_directory)
-else:
-  model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-  model.save_pretrained(model_directory)
+# model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+# model.save_pretrained(model_directory)
 
-# read transcript from file
+model = SentenceTransformer(model_directory)
+
 f = open(transcript_path, encoding='utf-8')
 transcript = f.read()
 f.close()
+transcript = re.sub('’', "'", transcript)
 
+WORD = r"[a-z]+(?:'[a-z]+)?"  # this ignores nummeric numbers since ForceAlign does
+
+# tokenizer = RegexpTokenizer(r"\b[\w']+\b")
+tokenizer = RegexpTokenizer(WORD)
 
 # remove_timestamps = re.sub(r"([0-9]{2}:){2}[0-9]{2}.[0-9]{3}", "", transcript)
-remove_timestamps = re.sub(r"\[(.*?)\]", "", transcript)
+remove_timestamps = re.sub(r"\[(.*?)\]", "", transcript.lower())
 remove_newlines = re.sub(r"\n", "", remove_timestamps)
 remove_parenthesis = re.sub(r"\([^)]*\)", "", remove_newlines)
+
+clean_transcript = ' '.join(tokenizer.tokenize(remove_parenthesis))
+tokenized_transcript = tokenizer.tokenize(clean_transcript)
 sentences = np.array(re.split(r"[.?!] *", remove_parenthesis), dtype=str)
+
+word_count = len(tokenized_transcript)
+
+# perform force alignment #
+alignment = ForceAlign(audio_file=audio_path, transcript=clean_transcript)
+word_times = alignment.inference()
+
+# # calculating times when no speech is present #
+# silence_threshold = 0.500
+# prev_word_end_time = word_times[1].time_start
+# silent_sections = []
+# for word in word_times:
+#     if word.time_start > prev_word_end_time + silence_threshold:
+#       silent_sections.append((prev_word_end_time+silence_threshold/2, word.time_start-silence_threshold/2))
+#     prev_word_end_time = word.time_end
+# total_silent_time = sum(map(lambda x: x[0]-x[1], silent_sections))
 
 # get sentence word counts
 sentence_word_counts = []
 for i in sentences:
-  word_count = len(re.findall(r"\b[\w']+\b", i))
-  sentence_word_counts.append(word_count)
+  # wc = len(re.findall(r"\b[\w']+\b", i))
+  wc = len(re.findall(WORD, i))
+  sentence_word_counts.append(wc)
 
 # embed sentences into the huggingface embedding
 embeddings = np.array(model.encode(sentences))
@@ -66,19 +89,29 @@ sentence_similarities = []
 N = 5
 min_x = N
 max_x = len(embeddings)-N
-for i in sentence_word_counts[:N]:
+for i in range(N):
   sentence_similarities += [0.0] * i
 for i in range(min_x, max_x):
   sentence_similarities += [measure_similarity(i, N)] * sentence_word_counts[i]
+for i in range(len(tokenized_transcript) - max_x):
+  sentence_similarities += [0.0] * i
 
 
-# get vocab data for encoding #
-with open("Models/vocab.pkl", "rb") as f:
+# measuring word probabilities #
+ngram = 10
+with open("Model_Data/vocab.pkl", "rb") as f:
   vocab = pickle.load(f)
+with open(f"Model_Data/X_test_{ngram}-gram.pkl", "rb") as f:
+  X_test = pickle.load(f)
+with open(f"Model_Data/y_test_{ngram}-gram.pkl", "rb") as f:
+  y_test = pickle.load(f)
+
+
 word_to_idx = {word: i for i, word in enumerate(vocab)}
 idx_to_word = {i: word for word, i in word_to_idx.items()}
 vocab_count = len(vocab)
-device_gpu = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device_cpu = torch.device("cpu")
+
 
 class NGramModel(nn.Module):
   def __init__(self, vocab_size, embed_dim, hidden_dim, num_layers=2):
@@ -102,17 +135,15 @@ word_predictor_model = NGramModel(
   hidden_dim=128,
   num_layers=1
 )
-word_predictor_model.to(device_gpu)
-# measuring word probabilities. This uses a previously trained n-grams model #
-ngram = 10
-with open(f"Models/trained_model_{ngram}-gram.pkl", "rb") as f:
+with open(f"Model_Data/trained_model_{ngram}-gram.pkl", "rb") as f:
   word_predictor_model = pickle.load(f)
+word_predictor_model.to(device_cpu)
 
 def predict_top_k_words(context_words, k=10, temperature=1.0):
   for w in context_words:
     if w not in word_to_idx:
       return []
-  context_idxs = torch.tensor([[word_to_idx[w] for w in context_words]], device=device_gpu)
+  context_idxs = torch.tensor([[word_to_idx[w] for w in context_words]], device=device_cpu)
   word_predictor_model.eval()
   with torch.no_grad():
     logits = word_predictor_model(context_idxs).squeeze(0) / temperature
@@ -122,14 +153,11 @@ def predict_top_k_words(context_words, k=10, temperature=1.0):
 
   top_probabilities = top_probabilities.cpu()
   top_indices = top_indices.cpu()
+  # return [(idx_to_word[i.item()]) for i in top_indices]
   return [(idx_to_word[i.item()], p.item()) for i, p in zip(top_indices, top_probabilities)]
 
 
 # iterate over transcript and calculate word probabilities
-tokenizer = RegexpTokenizer(r"[a-z]+(?:'[a-z]+)?")
-clean_transcript = tokenizer.tokenize(re.sub(r"\([^)]*\)", "", transcript.lower()))
-clean_transcript = ' '.join(clean_transcript)
-tokenized_transcript = tokenizer.tokenize(re.sub(r"\([^)]*\)", "", transcript.lower()))
 
 # Word probabilities
 word_probability_list = [(tokenized_transcript[0], 0.0), (tokenized_transcript[1], 0.0), (tokenized_transcript[2], 0.0), (tokenized_transcript[3], 0.0), (tokenized_transcript[4], 0.0), (tokenized_transcript[5], 0.0), (tokenized_transcript[6], 0.0), (tokenized_transcript[7], 0.0), (tokenized_transcript[8], 0.0)]
@@ -145,27 +173,7 @@ for i in range((ngram-1), len(tokenized_transcript)):
   word_probability_list.append((actual_next_word, word_probability))
 
 
-# perform force alignment and compute base speaking rate #
-alignment = ForceAlign(audio_file=audio_path, transcript=clean_transcript)
-word_times = alignment.inference()
-
-# # calculating times when no speech is present #
-# silence_threshold = 0.500
-# prev_word_end_time = word_times[1].time_start
-# silent_sections = []
-# for word in word_times:
-#     if word.time_start > prev_word_end_time + silence_threshold:
-#       silent_sections.append((prev_word_end_time+silence_threshold/2, word.time_start-silence_threshold/2))
-#     prev_word_end_time = word.time_end
-# total_silent_time = sum(map(lambda x: x[0]-x[1], silent_sections))
-
-word_count = len(word_times)
-total_speaking_time = word_times[word_count-1].time_start - word_times[0].time_end #- total_silent_time
-base_speaking_rate_minutes = word_count * (60 / total_speaking_time)
-
-base_speed_increase = 280 / base_speaking_rate_minutes
-dynamic_speed_multiplier = 50 / base_speaking_rate_minutes
-wordspeed_deltas = [ word_probability_list[i][1] + sentence_similarities[i] for i in range(len(sentence_similarities)) ]
+wordspeed_deltas = [ word_probability_list[i][1] + sentence_similarities[i] for i in range(word_count) ]
 
 
 # save word speed and time data to be used in later script
